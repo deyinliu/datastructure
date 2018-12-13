9c26a86ef104e94213957c7de2ec6defb9baa2bd
deyinliu <deyinliu@126.com>
2018-12-13 19:54:31 +0800

    最近比较忙，没刷pat，结果pat打不开了，转leetcode

--- chapter1/bubbleSort.cpp
+++ chapter1/bubbleSort.cpp
@@ -6,24 +6,20 @@ template<class T>
 void bubbleSort(T &a){
     int len = count(a);
     int isSort = false;
-    for(int i = 0; i < len && !isSort; i++)
-    {
+    for(int i = 0; i < len && !isSort; i++){
         isSort = true;
-        for(int j = 0; j < len-i-2 ; j++)
-        {
+        for(int j = 0; j < len-i-1 ; j++){
             if(a[j]>a[j+1]){
                 std::swap(a[j] , a[j+1]);
                 isSort = false;
             }
-        }
-        
-    }
-    
+        }  
+    } 
 }
 
 int main(int argc, char const *argv[])
 {
-    int a[] = {2,3,5,1,3};
+    int a[] = {2,4,1,1,3};
     bubbleSort(a);
     print(a);
     return 0;
