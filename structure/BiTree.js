6ea7f24bf504d9b7c9570243a71089d9fc6a908b
deyinliu <deyinliu@126.com>
2019-01-22 11:24:49 +0800

    edit BiTree

--- structure/BiTree.js
+++ structure/BiTree.js
@@ -1,3 +1,4 @@
+const Stack = require("./Stack").Stack;
 class Node {
     constructor(data, left = null, right = null, parent = null) {
         this.data = data;
@@ -26,15 +27,71 @@ class BiTree {
             }
         }
     }
-    inOrderRecur(head) {
-        // let head = this.root;
-        if (head == null) {
-            return;
+    inOrederRecur() {
+        let root = this.root;
+        if (!root) {
+            return null;
         }
-        this.inOrderRecur(head.left);
-        this.str += head.data + " ";
-        this.inOrderRecur(head.right);
-
+        let stack = new Stack();
+        let cur = root;
+        let str = "";
+        while (!stack.isEmpty() || cur != null) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                cur = stack.pop();
+                str += cur.data + ",";
+                cur = cur.right;
+            }
+        }
+        return str.slice(0, -1);
+    }
+    preOrderRecur() {
+        let root = this.root;
+        if (!root) {
+            return null;
+        }
+        let stack = new Stack();
+        let str = "";
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            let head = stack.pop();
+            str += head.data + ",";
+            if (head.right) {
+                stack.push(head.right);
+            }
+            if (head.left) {
+                stack.push(head.left);
+            }
+        }
+        return str.slice(0, -1);
+    }
+    posOrderRecur(root) {
+        let root = this.root;
+        if (!root) {
+            return null;
+        }
+        let s1 = new Stack();
+        let s2 = new Stack();
+        let cur = root;
+        let str = "";
+        s1.push(cur);
+        while (!s1.isEmpty()) {
+            cur = s1.pop();
+            s2.push(cur);
+            if (cur.left) {
+                s1.push(cur.left);
+            }
+            if (cur.right) {
+                s1.push(cur.right);
+            }
+        }
+        while (!s2.isEmpty()) {
+            cur = s2.pop();
+            str += cur.data + ",";
+        }
+        return str.slice(0, -1);
     }
     _position(cur, data) {
         let point = null;
