7690474622ea8c86f4bcff252b8f36ba9b2fc0b1
deyinliu <deyinliu@126.com>
2019-01-24 19:59:51 +0800

    层序遍历序列化&反序列化

--- interview/serialByLevel.js
+++ interview/serialByLevel.js
@@ -1,5 +1,12 @@
 const Queue = require("../structure/Queue").Queue;
 const BiTree = require("../structure/BiTree").BiTree;
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
 
 function serialByLevel(head) {
     if (!head) {
@@ -27,4 +34,38 @@ function serialByLevel(head) {
 }
 const tree = new BiTree();
 [4, 2, 1, 3, 5, 3, 2, 6].forEach(val => tree.add(val));
-console.log(serialByLevel(tree.root));
+let str = serialByLevel(tree.root)
+console.log(str);
+
+function reconByLevelString(arr) {
+    let val = arr.shift();
+    let head = generateNode(val);
+    let queue = new Queue();
+    if (head) {
+        queue.enqueue(head);
+    }
+    let node = null;
+    while (!queue.isEmpty()) {
+        node = queue.dequeue();
+        node.left = generateNode(arr.shift());
+        node.right = generateNode(arr.shift());
+        if (node.left) {
+            queue.enqueue(node.left);
+        }
+        if (node.right) {
+            queue.enqueue(node.right);
+        }
+    }
+    return head;
+}
+
+function generateNode(val) {
+    if (val == "#") {
+        return null;
+    }
+    return new Node(val);
+}
+
+let arr = str.split("!");
+let head = reconByLevelString(arr);
+console.log(serialByLevel(head));
